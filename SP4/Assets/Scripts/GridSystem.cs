358631e96efb5a62932e4099eae360aca33ed763
CynicalSteve <syong789@hotmail.com>
2018-02-12 18:09:29 +0800

    Starting on Grid System -Added some art assets

---
+++ SP4/Assets/Scripts/GridSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour {
+
+    Sprite[] a = new Sprite[100];
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
