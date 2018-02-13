d39a808e33354d14de0193c0e474511645749128
SIDM\160853T <greatreaperzsnip3@gmail.com>
2018-02-13 13:52:28 +0800

    -added GoldSystem script with addGold and getGold function

---
+++ SP4/Assets/Scripts/GoldSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldSystem : MonoBehaviour {
+
+    int gold = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void addGold(int _amount)
+    {
+        gold += _amount;
+    }
+    int getGold()
+    {
+        return gold;
+    }
+}
