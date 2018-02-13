c267be1b21ad71dee9d72b2fae7d181e0b003b41
SIDM\160853T <greatreaperzsnip3@gmail.com>
2018-02-13 12:34:11 +0800

    - Simple Main Menu done - updated build settings to allow scene transition(between Menu and Game scene) - added scene transition script

---
+++ SP4/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+
+	// Use this for initialization
+	//void Start () {
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+	//}
+
+    public void TransitToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void TransitToGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+}
