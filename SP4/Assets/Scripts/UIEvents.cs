79dac2d4f87d25aae12327aec3ad11221df11e4b
SIDM\160853T <greatreaperzsnip3@gmail.com>
2018-02-13 16:16:02 +0800

    - Added pause overlay in Game scene

---
+++ SP4/Assets/Scripts/UIEvents.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEvents : MonoBehaviour {
+    //void Start()
+    //{
+    //    Debug.Log("Hello");
+    //}
+    public void PauseMenuEnable(Animator _anim)
+    {
+        Debug.Log("Pausing");
+        _anim.SetBool("PauseEnabled", true);
+    }
+    public void PauseMenuDisable(Animator _anim)
+    {
+        Debug.Log("unPausing");
+        _anim.SetBool("PauseEnabled", false);
+    }
+    public void DebugLog(string text)
+    {
+        Debug.Log(text);
+    }
+}
