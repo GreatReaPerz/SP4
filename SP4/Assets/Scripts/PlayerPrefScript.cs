a47a9f0d3baa928f47f9d5054190d56576ffa4b1
ShishKebab99 <shishanth.kaliannan@gmail.com>
2018-02-14 00:30:46 +0800

    added a volume slider script and some music and also added a playerPrefscript to save all the player stats and volume to playerPref

---
+++ SP4/Assets/Scripts/PlayerPrefScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPrefScript : MonoBehaviour {
+
+    private float health = 100.0f;
+
+    private int gold = 0;
+
+    private float volume = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        health = PlayerPrefs.GetFloat("Health", health);
+        volume = PlayerPrefs.GetFloat("Volume", volume);
+        gold = PlayerPrefs.GetInt("Gold", gold);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SP4/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour {
+
+    public Slider volumeSlider;
+    public AudioSource volumeAudio;
+
+    // Use this for initialization
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+        volumeAudio.volume = volumeSlider.value;
+        PlayerPrefs.SetFloat("Volume", volumeSlider.value);
+        PlayerPrefs.Save();
+    }
+}
