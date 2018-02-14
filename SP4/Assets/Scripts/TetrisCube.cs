94976f331b699850eb61346b0ee9f10a0372a31f
LOO CHUAN XU <162551P@sidm.student.local>
2018-02-14 14:45:25 +0800

    -Made a 4x4 cube as a prefab and its logics as another gameobject which is also a gameobject -Trying to get spawning right

---
+++ SP4/Assets/Scripts/TetrisCube.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisCube : MonoBehaviour {
+
+	[SerializeField]
+	private Rigidbody2D btmLeft;
+	[SerializeField]
+	private Rigidbody2D btmRight;
+	[SerializeField]
+	private Rigidbody2D topLeft;
+	[SerializeField]
+	private Rigidbody2D topRight;
+
+	private Hashtable tetrisList = new Hashtable();
+	FixedJoint2D container = new FixedJoint2D();
+	private Rigidbody2D bottomRight = new Rigidbody2D ();
+		// Use this for initialization
+	void Start () {
+		container = btmLeft.GetComponent<FixedJoint2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void DragbtmLeft()
+	{
+		btmLeft.MovePosition (Input.mousePosition);
+	}
+
+	public void DragbtmRight()
+	{
+		btmRight.MovePosition (Input.mousePosition);
+	}
+
+	public void DragtopLeft()
+	{
+		topLeft.MovePosition(Input.mousePosition);
+	}
+
+	public void DragtopRight()
+	{
+		topRight.MovePosition (Input.mousePosition);
+	}
+		
+	public void AddTetrisCube(int key, GameObject value)
+	{
+		tetrisList.Add (key,value);
+	}
+
+	public object GetTetrisCube(int key)
+	{
+		if (tetrisList.ContainsKey (key))
+			return tetrisList [key];
+		else
+			return null;
+	}
+
+}
---
+++ SP4/Assets/Scripts/TetrisSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TetrisSpawner : MonoBehaviour {
+
+	[SerializeField]
+	GameObject[] TetrisList;
+
+  	// Use this for initialization
+	void Start () {
+		GameObject newCube = Instantiate (TetrisList [0], transform.position, Quaternion.identity);
+		newCube.transform.SetParent (GameObject.FindGameObjectWithTag ("Canvas").transform, true);
+		/*TetrisCube cube = new TetrisCube ();
+		cube.AddTetrisCube (1, newCube);
+		newCube =  (GameObject)cube.GetTetrisCube (1);
+		EventTrigger trigger = newCube.transform.Find("BtmLeft").GetComponent<EventTrigger> ();
+		EventTrigger.Entry entry = new EventTrigger.Entry ();
+		entry.eventID = EventTriggerType.Drag;
+		entry.callback.AddListener ((data) => {
+			cube.DragbtmLeft ();
+		});
+		trigger.triggers.Add (entry);*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
