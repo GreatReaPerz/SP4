105dfa11eb1a2367886ee2921b99982cda075b9f
CynicalSteve <syong789@hotmail.com>
2018-02-14 11:01:09 +0800

    Completed storing data of tetris block into grid

--- SP4/Assets/Scripts/GridSystem.cs
+++ SP4/Assets/Scripts/GridSystem.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GridSystem : MonoBehaviour {
-    RectTransform panelRectTransform;
 
     const ushort row = 6, col = 10;
-    const uint gridSize = row * col;
+    public const uint gridSize = row * col;
 
     const float tileWidth = 100;
     const float tileHeight = 100;
 
     private Tetris tetrisBlock = null;
+    private GridData theGridData = null;
 
     float halfTileWidth = tileWidth * 0.5f, halfTileHeight = tileHeight * 0.5f;
 
@@ -21,13 +21,15 @@ public class GridSystem : MonoBehaviour {
 
     [SerializeField]
     Image FirstTetrisBlock;
-
-    Canvas theCanvas;
     
     // Use this for initialization
     void Start () {
         tetrisBlock =  GameObject.Find("RedQuad").GetComponent<Tetris>();
+        theGridData = new GridData();
+        theGridData.Init();
+
         Debug.Assert(tetrisBlock != null);
+        Debug.Assert(theGridData != null);
 
         uint rowCount = 1, heightCount = 1;
         
@@ -47,12 +49,16 @@ public class GridSystem : MonoBehaviour {
                 heightCount += 2;
             }
 
+            //Anchor to bottom
             grid[i].rectTransform.anchorMin = new Vector2(0.5f, 0);
             grid[i].rectTransform.anchorMax = new Vector2(0.5f, 0);
             grid[i].rectTransform.pivot = new Vector2(0.5f, 0.5f);
         }
 
     }
+
+    private uint objectIndex = 0;
+    bool isMouseMovingAnObject = false;
 
     // Update is called once per frame
     void Update()
@@ -66,7 +72,6 @@ public class GridSystem : MonoBehaviour {
                 //Check col
                 for (uint it = 0; it < col; ++it)
                 {
-                    float gh = grid[0].transform.position.x + ((tileWidth * (it + 1)) - halfTileWidth);
                     if (grid[0].transform.position.x + ((tileWidth * (it + 1)) - halfTileWidth) > Input.mousePosition.x)
                     {
                         colNum = it;
@@ -77,7 +82,6 @@ public class GridSystem : MonoBehaviour {
                 //Check row
                 for (uint it = 0; it < row; ++it)
                 {
-                    float gh = grid[0].transform.position.y + ((tileHeight * (it + 1)) - halfTileHeight);
                     if (grid[0].transform.position.y + ((tileHeight * (it + 1)) - halfTileHeight) > Input.mousePosition.y)
                     {
                         rowNum = it;
@@ -85,19 +89,114 @@ public class GridSystem : MonoBehaviour {
                     }
                 }
 
-                uint rg = colNum + (rowNum * col);
-                Debug.Log(rg.ToString());
-                Debug.Log("X: " + grid[rg].transform.position.x.ToString() + " Y: " + grid[rg].transform.position.y.ToString());
-                Debug.Log("MouseX: " + Input.mousePosition.x + " MouseY: " + Input.mousePosition.y);
-                FirstTetrisBlock.transform.position = grid[colNum + (rowNum * col)].transform.position;
+                objectIndex = colNum + (rowNum * col);
+                FirstTetrisBlock.transform.position = grid[objectIndex].transform.position;
+                isMouseMovingAnObject = true;
             }
 
         }
-        else
+        else if (isMouseMovingAnObject && Input.GetMouseButtonUp(0)) //When the touch is released, it saves the tetris data into the data grid tile
         {
+            //Set the tetris block to not moving
             tetrisBlock.isMoving = false;
-        }
 
+            //Pass the tetris block data into the grid data
+            if(theGridData.AddTetrisBlockData(objectIndex, "Test01", "UnitTest", 100, 20, 35, 45))
+            {
+                Debug.Log("Successful Save");
+            }
+            else
+            {
+                Debug.Log("Unsuccessful Save");
+            }
+        }
+        
+        //When the tetris block is picked up from the grid, it removes the data from that tile
+        if (isMouseMovingAnObject && Input.GetMouseButtonDown(0))
+        {
+            //Remove the data from the tile
+            if (theGridData.RemoveTetrisBlockData(objectIndex))
+            {
+                Debug.Log("Successful Removal");
+            }
+            else
+            {
+                Debug.Log("Unsuccessful Removal");
+            }
+
+            isMouseMovingAnObject = false;
+        }
     }
 }
+
+//The grid where data is stored
+public class GridData
+{
+    public TetrisData[] gridData;
+    uint gridDataSize;
+    
+    public void Init()
+    {
+        gridDataSize = GridSystem.gridSize;
+        gridData = new TetrisData[gridDataSize];
+    }
+
+    void Update()
+    {
+    }
+
+    public bool AddTetrisBlockData(uint Index, string NameID, string UnitType, uint Health, uint moveSpeed, uint attackDamage, uint attackRate)
+    {
+        if (gridData[Index] != null)
+        {
+            return false;
+        }
+
+        gridData[Index] = new TetrisData();
+        gridData[Index].CreateUnit(NameID, UnitType, Health, moveSpeed, attackDamage, attackRate);
+        
+        return true;
+    }
+
+    public bool RemoveTetrisBlockData(uint Index)
+    {
+        if (gridData[Index] == null)
+        {
+            return false;
+        }
+
+        gridData[Index] = null;
+
+        return true;
+    }
+}
+
+//Storage class for Tetris Block data
+public class TetrisData
+{
+    public string NameID, UnitType;
+    uint Health, moveSpeed, attackDamage, attackRate;
+
+
+    public TetrisData()
+    {
+        NameID = UnitType = "";
+        Health = moveSpeed = attackDamage = attackRate = 0;
+    }
+
+    void Update()
+    {
+    }
+
+    public void CreateUnit(string NameID, string UnitType, uint Health, uint moveSpeed, uint attackDamage, uint attackRate)
+    {
+        this.NameID = NameID;
+        this.UnitType = UnitType;
+        this.Health = Health;
+        this.moveSpeed = moveSpeed;
+        this.attackDamage = attackDamage;
+        this.attackRate = attackRate;
+    }
+}
+
 
