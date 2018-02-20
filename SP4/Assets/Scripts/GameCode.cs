f53fcf74b5c8148c4efe566be3b0c28cd3beff17
SIDM\161953p <shishanth.kaliannan@gmail.com>
2018-02-20 15:09:21 +0800

    added states

--- SP4/Assets/Scripts/GameCode.cs
+++ SP4/Assets/Scripts/GameCode.cs
@@ -44,171 +44,179 @@ public class GameCode : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(timer > 5.0f && !destroyed)
+        if(state == (int)GameState.PLANNING)
         {
-            for (int i = 0; i < 3; ++i)
+            if (timer > 5.0f && !destroyed)
             {
-                if (theTetrisSpawner.tetrisList[i].sav)
+                for (int i = 0; i < 3; ++i)
                 {
-                    GameObject newObj;
-                    Vector3 hello = theTetrisSpawner.tetrisList[i].partOne.transform.position;
-                    hello.z = 100;
-                    Debug.Log(theTetrisSpawner.tetrisList[i].troopName);
-                    newObj = (GameObject)Instantiate(GameObject.Find(theTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
-                    newObj.transform.parent = Ui.transform;
-                    troop = newObj.GetComponent<TroopAI>();
-                    troop.team = 1;
-                    troop.terrainName = TerrainName;
-                    objects.Add(newObj);
+                    if (theTetrisSpawner.tetrisList[i].sav)
+                    {
+                        GameObject newObj;
+                        Vector3 hello = theTetrisSpawner.tetrisList[i].partOne.transform.position;
+                        hello.z = 100;
+                        Debug.Log(theTetrisSpawner.tetrisList[i].troopName);
+                        newObj = (GameObject)Instantiate(GameObject.Find(theTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
+                        newObj.transform.parent = Ui.transform;
+                        troop = newObj.GetComponent<TroopAI>();
+                        troop.team = 1;
+                        troop.terrainName = TerrainName;
+                        objects.Add(newObj);
 
-                    GameObject newObj1;
-                    hello = theTetrisSpawner.tetrisList[i].partTwo.transform.position;
-                    hello.z = 100;
-                    newObj1 = (GameObject)Instantiate(GameObject.Find(theTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
-                    newObj1.transform.parent = Ui.transform;
-                    troop = newObj1.GetComponent<TroopAI>();
-                    troop.team = 1;
-                    troop.terrainName = TerrainName;
-                    objects.Add(newObj1);
+                        GameObject newObj1;
+                        hello = theTetrisSpawner.tetrisList[i].partTwo.transform.position;
+                        hello.z = 100;
+                        newObj1 = (GameObject)Instantiate(GameObject.Find(theTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
+                        newObj1.transform.parent = Ui.transform;
+                        troop = newObj1.GetComponent<TroopAI>();
+                        troop.team = 1;
+                        troop.terrainName = TerrainName;
+                        objects.Add(newObj1);
 
-                    GameObject newObj2;
-                    hello = theTetrisSpawner.tetrisList[i].partThree.transform.position;
-                    hello.z = 100;
-                    newObj2 = (GameObject)Instantiate(GameObject.Find(theTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
-                    newObj2.transform.parent = Ui.transform;
-                    troop = newObj2.GetComponent<TroopAI>();
-                    troop.team = 1;
-                    troop.terrainName = TerrainName;
-                    objects.Add(newObj2);
+                        GameObject newObj2;
+                        hello = theTetrisSpawner.tetrisList[i].partThree.transform.position;
+                        hello.z = 100;
+                        newObj2 = (GameObject)Instantiate(GameObject.Find(theTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
+                        newObj2.transform.parent = Ui.transform;
+                        troop = newObj2.GetComponent<TroopAI>();
+                        troop.team = 1;
+                        troop.terrainName = TerrainName;
+                        objects.Add(newObj2);
 
-                    GameObject newObj3;
-                    hello = theTetrisSpawner.tetrisList[i].partFour.transform.position;
-                    hello.z = 100;
-                    newObj3 = (GameObject)Instantiate(GameObject.Find(theTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
-                    newObj3.transform.parent = Ui.transform;
-                    troop = newObj3.GetComponent<TroopAI>();
-                    troop.team = 1;
-                    troop.terrainName = TerrainName;
-                    objects.Add(newObj3);
+                        GameObject newObj3;
+                        hello = theTetrisSpawner.tetrisList[i].partFour.transform.position;
+                        hello.z = 100;
+                        newObj3 = (GameObject)Instantiate(GameObject.Find(theTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
+                        newObj3.transform.parent = Ui.transform;
+                        troop = newObj3.GetComponent<TroopAI>();
+                        troop.team = 1;
+                        troop.terrainName = TerrainName;
+                        objects.Add(newObj3);
+                    }
                 }
-            }
-            for (int i = 0; i < 3; ++i)
-            {
-                if (enemyTetrisSpawner.tetrisList[i].sav)
+                for (int i = 0; i < 3; ++i)
                 {
-                    GameObject newObj;
-                    Vector3 hello = enemyTetrisSpawner.tetrisList[i].partOne.transform.position;
-                    hello.z = 100;
-                    newObj = (GameObject)Instantiate(GameObject.Find(enemyTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
-                    newObj.transform.parent = Ui.transform;
-                    troop = newObj.GetComponent<TroopAI>();
-                    troop.team = -1;
-                    troop.terrainName = TerrainName;
-                    objects.Add(newObj);
+                    if (enemyTetrisSpawner.tetrisList[i].sav)
+                    {
+                        GameObject newObj;
+                        Vector3 hello = enemyTetrisSpawner.tetrisList[i].partOne.transform.position;
+                        hello.z = 100;
+                        newObj = (GameObject)Instantiate(GameObject.Find(enemyTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
+                        newObj.transform.parent = Ui.transform;
+                        troop = newObj.GetComponent<TroopAI>();
+                        troop.team = -1;
+                        troop.terrainName = TerrainName;
+                        objects.Add(newObj);
 
-                    GameObject newObj1;
-                    hello = enemyTetrisSpawner.tetrisList[i].partTwo.transform.position;
-                    hello.z = 100;
-                    newObj1 = (GameObject)Instantiate(GameObject.Find(enemyTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
-                    newObj1.transform.parent = Ui.transform;
-                    troop = newObj1.GetComponent<TroopAI>();
-                    troop.team = -1;
-                    troop.terrainName = TerrainName;
-                    objects.Add(newObj1);
+                        GameObject newObj1;
+                        hello = enemyTetrisSpawner.tetrisList[i].partTwo.transform.position;
+                        hello.z = 100;
+                        newObj1 = (GameObject)Instantiate(GameObject.Find(enemyTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
+                        newObj1.transform.parent = Ui.transform;
+                        troop = newObj1.GetComponent<TroopAI>();
+                        troop.team = -1;
+                        troop.terrainName = TerrainName;
+                        objects.Add(newObj1);
 
-                    GameObject newObj2;
-                    hello = enemyTetrisSpawner.tetrisList[i].partThree.transform.position;
-                    hello.z = 100;
-                    newObj2 = (GameObject)Instantiate(GameObject.Find(enemyTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
-                    newObj2.transform.parent = Ui.transform;
-                    troop = newObj2.GetComponent<TroopAI>();
-                    troop.team = -1;
-                    troop.terrainName = TerrainName;
-                    objects.Add(newObj2);
+                        GameObject newObj2;
+                        hello = enemyTetrisSpawner.tetrisList[i].partThree.transform.position;
+                        hello.z = 100;
+                        newObj2 = (GameObject)Instantiate(GameObject.Find(enemyTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
+                        newObj2.transform.parent = Ui.transform;
+                        troop = newObj2.GetComponent<TroopAI>();
+                        troop.team = -1;
+                        troop.terrainName = TerrainName;
+                        objects.Add(newObj2);
 
-                    GameObject newObj3;
-                    hello = enemyTetrisSpawner.tetrisList[i].partFour.transform.position;
-                    hello.z = 100;
-                    newObj3 = (GameObject)Instantiate(GameObject.Find(enemyTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
-                    newObj3.transform.parent = Ui.transform;
-                    troop = newObj3.GetComponent<TroopAI>();
-                    troop.team = -1;
-                    troop.terrainName = TerrainName;
-                    objects.Add(newObj3);
+                        GameObject newObj3;
+                        hello = enemyTetrisSpawner.tetrisList[i].partFour.transform.position;
+                        hello.z = 100;
+                        newObj3 = (GameObject)Instantiate(GameObject.Find(enemyTetrisSpawner.tetrisList[i].troopName), hello, Quaternion.identity);
+                        newObj3.transform.parent = Ui.transform;
+                        troop = newObj3.GetComponent<TroopAI>();
+                        troop.team = -1;
+                        troop.terrainName = TerrainName;
+                        objects.Add(newObj3);
+                    }
                 }
-            }
-
-            for (int i = 0; i < 3; ++i)
-            {
-                Destroy(theTetrisSpawner.tetrisList[i].parentCube);
-                Destroy(enemyTetrisSpawner.tetrisList[i].parentCube);
-                destroyed = true;
-            }
+                for (int i = 0; i < 3; ++i)
+                {
+                    Destroy(theTetrisSpawner.tetrisList[i].parentCube);
+                    Destroy(enemyTetrisSpawner.tetrisList[i].parentCube);
+                    destroyed = true;
+                }
 
                 Destroy(theGridSystem);
                 Destroy(enemyGridSystem);
 
-            timer = 0.0f;
-        }
-        if (objects.Count <= 0 && destroyed)
-        {
-            theTetrisSpawner.Start();
-            theGridSystem.Start();
-            enemyTetrisSpawner.Start();
-            enemyGridSystem.Start();
-            Terrain.GetComponent<MainGame>().Start();
-            TerrainName = Terrain.GetComponent<MainGame>().NeutralZoneTerrainType;
-            destroyed = false;
-            timer = 0.0f;
+                timer = 0.0f;
+                state = (int)GameState.ATTACK;
+            }
+            if (!destroyed)
+            {
+                theGridSystem.GameUpdate();
+                enemyGridSystem.GameUpdate();
+            }
         }
 
-        if (!destroyed)
+        if (state == (int)GameState.ATTACK)
         {
-            theGridSystem.GameUpdate();
-            enemyGridSystem.GameUpdate();
-        }
-        if (destroyed)
-        {
-            if(objects.Count > 0)
+            if (destroyed)
             {
-                for (int i = 0; i < objects.Count; ++ i)
+                if (objects.Count > 0)
                 {
-                    troop = objects[i].GetComponent<TroopAI>();
-                    if (troop.activ)
+                    for (int i = 0; i < objects.Count; ++i)
                     {
-                        if(troop.team == 1)
+                        troop = objects[i].GetComponent<TroopAI>();
+                        if (troop.activ)
                         {
-                            if(objects[i].transform.position.y > 2000)
+                            if (troop.team == 1)
                             {
-                                troop.activ = false;
-                                Player2.GetComponent<HealthSystem>().addHealth(-1);
+                                if (objects[i].transform.position.y > 2000)
+                                {
+                                    troop.activ = false;
+                                    Player2.GetComponent<HealthSystem>().addHealth(-1);
+                                }
                             }
-                        }
-                        if (troop.team == -1)
-                        {
-                            if (objects[i].transform.position.y < 100)
+                            if (troop.team == -1)
                             {
-                                troop.activ = false;
-                                Player1.GetComponent<HealthSystem>().addHealth(-1);
+                                if (objects[i].transform.position.y < 100)
+                                {
+                                    troop.activ = false;
+                                    Player1.GetComponent<HealthSystem>().addHealth(-1);
+                                }
                             }
                         }
                     }
-                }
-                for (int i = 0; i < objects.Count;)
-                {
-                    troop = objects[i].GetComponent<TroopAI>();
-                    if(troop.activ)
+                    for (int i = 0; i < objects.Count;)
                     {
-                        ++i;
-                    }
-                    else
-                    {
-                        Destroy(objects[i]);
-                        objects.RemoveAt(i);
+                        troop = objects[i].GetComponent<TroopAI>();
+                        if (troop.activ)
+                        {
+                            ++i;
+                        }
+                        else
+                        {
+                            Destroy(objects[i]);
+                            objects.RemoveAt(i);
+                        }
                     }
                 }
             }
+            if (objects.Count <= 0 && destroyed)
+            {
+                theTetrisSpawner.Start();
+                theGridSystem.Start();
+                enemyTetrisSpawner.Start();
+                enemyGridSystem.Start();
+                Terrain.GetComponent<MainGame>().Start();
+                TerrainName = Terrain.GetComponent<MainGame>().NeutralZoneTerrainType;
+                destroyed = false;
+                timer = 0.0f;
+                state = (int)GameState.PLANNING;
+            }
         }
+
         timer += 1.0f / 60.0f;
 	}
 }
