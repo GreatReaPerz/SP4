bea73737aa10eee535413454262640d4b86298d4
LOO CHUAN XU <162551P@sidm.student.local>
2018-02-21 14:10:59 +0800

    -Changed tetris spawner so that it shows the unit type the moment it spawns

--- SP4/Assets/Scripts/TetrisSpawner.cs
+++ SP4/Assets/Scripts/TetrisSpawner.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TetrisSpawner : MonoBehaviour{
 
 	[SerializeField]
 	GameObject[] TetrisTypes;
 
+    [SerializeField]
+    Sprite[] troopImages;
 
 	public TetrisCube[] tetrisList = new TetrisCube[3]; 
 	float timer = 0;
@@ -21,7 +24,7 @@ public class TetrisSpawner : MonoBehaviour{
 	public void Start () {
         numSpawned = 0;
         for (int i = 0; i < 3; ++i) {
-            int rand = 2; //Random.Range (0, TetrisTypes.Length);
+            int rand = Random.Range (0, TetrisTypes.Length);
 			switch(rand)
 			{
 			case 0:
@@ -74,26 +77,8 @@ public class TetrisSpawner : MonoBehaviour{
 	{
         TetrisCube theCube = new TetrisCube();
         theCube.parentCube = Instantiate (TetrisTypes [0], transform.position, Quaternion.identity);
-        
-        int rand = Random.Range(0, 3);
-        switch (rand)
-        {
-            case 0:
-                {
-                    theCube.troopName = "Cavalry";
-                    break;
-                }
-            case 1:
-                {
-                    theCube.troopName = "Infantry";
-                    break;
-                }
-            case 2:
-                {
-                    theCube.troopName = "Bowmen";
-                    break;
-                }
-        };
+
+      
         theCube.parentCube.transform.SetParent (GameObject.FindGameObjectWithTag ("Canvas").transform, true);
         pil.Set(-300 + (key * 300), -300, 0);
         pil1 = theCube.parentCube.transform.position + pil;
@@ -102,12 +87,47 @@ public class TetrisSpawner : MonoBehaviour{
 
         //Set up the 4 cubes based on theCube.parentCube's child
 		theCube.setTheCubes (theCube.parentCube.transform.Find ("partOne").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partTwo").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partThree").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partFour").GetComponent<Rigidbody2D> ());
-		theCube.setTheObjectType (TetrisCube.objectType.TETRIS_4X4); 
-	
-		//Could use raycast instead 
-		//Also cause the only thing changing is the movement function, could try to make a switch instead
-		//Trigger and entry for bottom left 
-		EventTrigger BtmLTrig= theCube.parentCube.transform.Find("partOne").GetComponent<EventTrigger> ();
+		theCube.setTheObjectType (TetrisCube.objectType.TETRIS_4X4);
+
+        int rand = Random.Range(0, 2);
+        switch (rand)
+        {
+            case 0:
+                {
+                    theCube.troopName = "Cavalry";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[0];
+                    }
+                    break;
+                }
+            case 1:
+                {
+                    theCube.troopName = "Infantry";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[1];
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    theCube.troopName = "Bowmen";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[2];
+                    }
+                    break;
+                }
+        };
+
+        //Could use raycast instead 
+        //Also cause the only thing changing is the movement function, could try to make a switch instead
+        //Trigger and entry for bottom left 
+        EventTrigger BtmLTrig= theCube.parentCube.transform.Find("partOne").GetComponent<EventTrigger> ();
 		EventTrigger.Entry BtmLEntry = new EventTrigger.Entry ();
 		BtmLEntry.eventID = EventTriggerType.Drag;
 		BtmLEntry.callback.AddListener ((data) => {
@@ -151,26 +171,6 @@ public class TetrisSpawner : MonoBehaviour{
 	public int SpawnTShape(int key)
 	{
         TetrisCube theCube = new TetrisCube();
-        int rand = Random.Range(0, 3);
-        switch (rand)
-        {
-            case 0:
-                {
-                    theCube.troopName = "Cavalry";
-                    break;
-                }
-            case 1:
-                {
-                    theCube.troopName = "Infantry";
-                    break;
-                }
-            case 2:
-                {
-                    theCube.troopName = "Bowmen";
-                    break;
-                }
-        };
-
         theCube.parentCube = Instantiate (TetrisTypes [2], transform.position, Quaternion.Euler(0,0,90));
 
         theCube.parentCube.transform.SetParent (GameObject.FindGameObjectWithTag ("Canvas").transform, true);
@@ -182,11 +182,47 @@ public class TetrisSpawner : MonoBehaviour{
 		//Set up the 4 cubes based on theCube.parentCube's child
 		theCube.setTheCubes (theCube.parentCube.transform.Find ("partOne").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partTwo").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partThree").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partFour").GetComponent<Rigidbody2D> ());
 		theCube.setTheObjectType (TetrisCube.objectType.TETRIS_T);
-		//Could use raycast instead 
-		//Also cause the only thing changing is the movement function, could try to make a switch instead
 
-		//Trigger and entry for bottom left 
-		EventTrigger BtmLTrig= theCube.parentCube.transform.Find("partOne").GetComponent<EventTrigger> ();
+        int rand = Random.Range(0, 2);
+        switch (rand)
+        {
+            case 0:
+                {
+                    theCube.troopName = "Cavalry";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[0];
+                    }
+                    break;
+                }
+            case 1:
+                {
+                    theCube.troopName = "Infantry";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[1];
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    theCube.troopName = "Bowmen";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[2];
+                    }
+                    break;
+                }
+        };
+
+        //Could use raycast instead 
+        //Also cause the only thing changing is the movement function, could try to make a switch instead
+
+        //Trigger and entry for bottom left 
+        EventTrigger BtmLTrig = theCube.parentCube.transform.Find("partOne").GetComponent<EventTrigger> ();
 		EventTrigger.Entry BtmLEntry = new EventTrigger.Entry ();
 		BtmLEntry.eventID = EventTriggerType.Drag;
 		BtmLEntry.callback.AddListener ((data) => {
@@ -230,25 +266,6 @@ public class TetrisSpawner : MonoBehaviour{
 	public int SpawnLShape(int key)
 	{
         TetrisCube theCube = new TetrisCube();
-        int rand = Random.Range(0, 3);
-        switch (rand)
-        {
-            case 0:
-                {
-                    theCube.troopName = "Cavalry";
-                    break;
-                }
-            case 1:
-                {
-                    theCube.troopName = "Infantry";
-                    break;
-                }
-            case 2:
-                {
-                    theCube.troopName = "Bowmen";
-                    break;
-                }
-        };
         theCube.parentCube = Instantiate (TetrisTypes [1], transform.position, Quaternion.identity);
 		theCube.parentCube.transform.SetParent (GameObject.FindGameObjectWithTag ("Canvas").transform, true);
 		pil.Set(-300 + (key * 300), -300, 0);
@@ -260,10 +277,45 @@ public class TetrisSpawner : MonoBehaviour{
 		theCube.setTheCubes (theCube.parentCube.transform.Find ("partOne").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partTwo").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partThree").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partFour").GetComponent<Rigidbody2D> ());
 		theCube.setTheObjectType (TetrisCube.objectType.TETRIS_L);
 
-		//Could use raycast instead 
-		//Also cause the only thing changing is the movement function, could try to make a switch instead
-		//Trigger and entry for bottom left 
-		EventTrigger BtmLTrig= theCube.parentCube.transform.Find("partOne").GetComponent<EventTrigger> ();
+        int rand = Random.Range(0, 2);
+        switch (rand)
+        {
+            case 0:
+                {
+                    theCube.troopName = "Cavalry";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[0];
+                    }
+                    break;
+                }
+            case 1:
+                {
+                    theCube.troopName = "Infantry";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[1];
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    theCube.troopName = "Bowmen";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[2];
+                    }
+                    break;
+                }
+        };
+
+        //Could use raycast instead 
+        //Also cause the only thing changing is the movement function, could try to make a switch instead
+        //Trigger and entry for bottom left 
+        EventTrigger BtmLTrig= theCube.parentCube.transform.Find("partOne").GetComponent<EventTrigger> ();
 		EventTrigger.Entry BtmLEntry = new EventTrigger.Entry ();
 		BtmLEntry.eventID = EventTriggerType.Drag;
 		BtmLEntry.callback.AddListener ((data) => {
@@ -307,25 +359,6 @@ public class TetrisSpawner : MonoBehaviour{
 	public int SpawnZShape(int key)
 	{
         TetrisCube theCube = new TetrisCube();
-        int rand = Random.Range(0, 3);
-        switch (rand)
-        {
-            case 0:
-                {
-                    theCube.troopName = "Cavalry";
-                    break;
-                }
-            case 1:
-                {
-                    theCube.troopName = "Infantry";
-                    break;
-                }
-            case 2:
-                {
-                    theCube.troopName = "Bowmen";
-                    break;
-                }
-        };
         theCube.parentCube = Instantiate (TetrisTypes [3], transform.position, Quaternion.identity);
 		theCube.parentCube.transform.SetParent (GameObject.FindGameObjectWithTag ("Canvas").transform, true);
 		pil.Set(-300 + (key * 300), -300, 0);
@@ -333,14 +366,50 @@ public class TetrisSpawner : MonoBehaviour{
 		theCube.origin.Set(pil1.x, pil1.y, pil1.z);
 		theCube.parentCube.transform.position = theCube.origin;
 
-
-		//Set up the 4 cubes based on theCube.parentCube's child
-		theCube.setTheCubes (theCube.parentCube.transform.Find ("partOne").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partTwo").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partThree").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partFour").GetComponent<Rigidbody2D> ());
+        //Set up the 4 cubes based on theCube.parentCube's child
+        theCube.setTheCubes (theCube.parentCube.transform.Find ("partOne").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partTwo").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partThree").GetComponent<Rigidbody2D> (), theCube.parentCube.transform.Find ("partFour").GetComponent<Rigidbody2D> ());
 		theCube.setTheObjectType (TetrisCube.objectType.TETRIS_Z);
-		//Could use raycast instead 
-		//Also cause the only thing changing is the movement function, could try to make a switch instead
-		//Trigger and entry for bottom left 
-		EventTrigger BtmLTrig= theCube.parentCube.transform.Find("partOne").GetComponent<EventTrigger> ();
+
+
+        int rand = Random.Range(0, 2);
+        switch (rand)
+        {
+            case 0:
+                {
+                    theCube.troopName = "Cavalry";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[0];
+                    }
+                    break;
+                }
+            case 1:
+                {
+                    theCube.troopName = "Infantry";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[1];
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    theCube.troopName = "Bowmen";
+                    foreach (Transform child in theCube.parentCube.transform)
+                    {
+                        Image theSprite = child.GetComponent<Image>();
+                        theSprite.sprite = troopImages[2];
+                    }
+                    break;
+                }
+        };
+
+        //Could use raycast instead 
+        //Also cause the only thing changing is the movement function, could try to make a switch instead
+        //Trigger and entry for bottom left 
+        EventTrigger BtmLTrig= theCube.parentCube.transform.Find("partOne").GetComponent<EventTrigger> ();
 		EventTrigger.Entry BtmLEntry = new EventTrigger.Entry ();
 		BtmLEntry.eventID = EventTriggerType.Drag;
 		BtmLEntry.callback.AddListener ((data) => {
